cf1e19fd32b54ea65d29f574be7a142c56423814
inquis1t0r <pi0tr3k9292@gmail.com>
2023-12-01 17:25:46 +0100

    FileReader String fix

--- AoC2023Swift/AoC2023Swift/main.swift
+++ AoC2023Swift/AoC2023Swift/main.swift
@@ -10,14 +10,18 @@ import Foundation
 print("Hello, World!")
 
 // Example usage:
-let filePath = "/Users/inquis1t0r/Documents/Development/AdventOfCode2023/AoC2023Swift/AoC2023Swift/Day1input.markdown"
+let filePath = "/Users/inquis1t0r/Documents/Development/AdventOfCode2023/AoC2023Swift/AoC2023Swift/Day1input.markdown" //TODO: Markdown -> .txt
 
 // Assuming your file contains integers, you can instantiate the FileReader like this:
-let fileReader = FileReader<Int>(filePath: filePath)
+//let fileReader = FileReader<Int>(filePath: filePath)
+let fileReader = FileReader<String>(filePath: filePath)
 
 // Read lines from the file
 if let lines = fileReader.readLines() {
-    print("Lines from file: \(lines)")
+    for line in lines {
+        print(line)
+    }
+    //print("Lines from file: \(lines)")
 } else {
     print("Failed to read lines from the file.")
 }
