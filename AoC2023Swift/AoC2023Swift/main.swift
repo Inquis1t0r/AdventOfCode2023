a230a911d43d913276494e057c5a334856056f20
inquis1t0r <pi0tr3k9292@gmail.com>
2023-12-02 13:09:02 +0100

    Day1 WiP solution

--- AoC2023Swift/AoC2023Swift/main.swift
+++ AoC2023Swift/AoC2023Swift/main.swift
@@ -19,9 +19,36 @@ let fileReader = FileReader<String>(filePath: filePath)
 // Read lines from the file
 if let lines = fileReader.readLines() {
     for line in lines {
-        print(line)
+        //print(line)
     }
     //print("Lines from file: \(lines)")
 } else {
     print("Failed to read lines from the file.")
 }
+
+func extractFirstAndLastDigits(from input: String) -> (Int?, Int?) {
+    // Filter out non-digit characters
+    let digits = input.compactMap { Int(String($0)) }
+    
+    // Check if there are any digits
+    guard !digits.isEmpty else {
+        return (nil, nil)
+    }
+    
+    // Return the first and last digits
+    return (digits.first, digits.last)
+}
+
+// Example usage:
+let example1 = "abc123def456"
+let example2 = "xyz789"
+let example3 = "noDigitsHere"
+
+let result1 = extractFirstAndLastDigits(from: example1)
+let result2 = extractFirstAndLastDigits(from: example2)
+let result3 = extractFirstAndLastDigits(from: example3)
+
+//TODO: unwrapping? 
+print("Example 1: \(result1)") // (1, 6)
+print("Example 2: \(result2)") // (7, 9)
+print("Example 3: \(result3)") // (nil, nil)
